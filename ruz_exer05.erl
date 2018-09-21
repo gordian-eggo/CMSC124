2c9470522204978733d4f28e120766a6a9b870c5
gordian-eggo <columbus.knot@gmail.com>
2018-09-22 02:15:02 +0800

    Made base case for factors(N).

--- ruz_exer05.erl
+++ ruz_exer05.erl
@@ -1,8 +1,14 @@
 -module(ruz_exer05).
--export([factors/0, safety/0]).
+-export([factors/1, safety/0]).
 
-factors()->
-	io:format("mumshie ayanashaaaaaa~n").
+factors(1)-> 										% base case for recursion
+	N_factors = [1],
+	io:format("factors: ~p~n", [N_factors]).		% use ~p to automagically print the list
+
+%factors(N)->										% print a list of all the factors of N
+%	if 
+	
+
 
 safety()->
 	io:format("wag ibenta ang bayan bc u aint a pussy like aguinaldo.~n").
