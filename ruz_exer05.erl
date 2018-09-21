63ea8a68cbd2b7239f5f6c9cd8b224999ed885f7
gordian-eggo <columbus.knot@gmail.com>
2018-09-22 02:55:56 +0800

    Made code body for factors(N), error in appending to factors list.

--- ruz_exer05.erl
+++ ruz_exer05.erl
@@ -1,13 +1,26 @@
 -module(ruz_exer05).
 -export([factors/1, safety/0]).
 
-factors(1)-> 										% base case for recursion
-	N_factors = [1],
-	io:format("factors: ~p~n", [N_factors]).		% use ~p to automagically print the list
+factors(1) -> 										% base case for recursion
+	Factor_List = [],
+	N_factors  = Factor_List ++ 1,
+	io:format("factors: ~p~n", [N_factors]);		% use ~p to automagically print the list. Or anything. 
 
-%factors(N)->										% print a list of all the factors of N
-%	if 
-	
+factors(N) ->										% print a list of all the factors of N
+	Factor_List = [],
+	N_factors = [],
+	Remainder = N rem (N-1),						    % find the remainder to determine if the value in 
+
+	io:format("N = ~p, (N-1) = ~p, Remainder: ~p~n", [N, N-1, Remainder]),
+	if
+		Remainder == 0 ->
+			io:format("here~n"),
+			N_factors = Factor_List ++ (N-1),
+			factors(N-1);
+		Remainder =/= 0 ->
+			io:format("then here~n"),
+			factors(N-1)
+	end.
 
 
 safety()->
