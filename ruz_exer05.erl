144fe990bc76d2edbc33bcefdd5045d7024f2ead
gordian-eggo <columbus.knot@gmail.com>
2018-09-22 03:12:02 +0800

    Added append() for list concat

--- ruz_exer05.erl
+++ ruz_exer05.erl
@@ -1,21 +1,20 @@
 -module(ruz_exer05).
 -export([factors/1, append/2, safety/0]).
 
-factors(1) -> 										% base case for recursion
-	Factor_List = [],
-	N_factors  = Factor_List ++ 1,
-	io:format("factors: ~p~n", [N_factors]);		% use ~p to automagically print the list. Or anything. 
+factors(1) -> 													% base case for recursion
+	Factor_List = [1],
+	io:format("(base case) factors: ~p~n", [Factor_List]);		% use ~p to automagically print the list. Or anything. 
 
-factors(N) ->										% print a list of all the factors of N
-	Factor_List = [],
-	N_factors = [],
-	Remainder = N rem (N-1),						    % find the remainder to determine if the value in 
+factors(N) ->												% print a list of all the factors of N
+	Factor_List = [1],
+	Remainder = N rem (N-1),							    % find the remainder to determine if the value in 
 
 	io:format("N = ~p, (N-1) = ~p, Remainder: ~p~n", [N, N-1, Remainder]),
 	if
 		Remainder == 0 ->
 			io:format("here~n"),
-			N_factors = Factor_List ++ (N-1),
+			append([Factor_List], (N-1)),					% append the value of N-1 to the list
+			io:format("factors: ~p~n", [Factor_List]),
 			factors(N-1);
 		Remainder =/= 0 ->
 			io:format("then here~n"),
