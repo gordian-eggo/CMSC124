459a113dd7414fb6498b85f5fbc5fee169153028
gordian-eggo <columbus.knot@gmail.com>
2018-09-22 19:41:08 +0800

    Used list comprehension for new factors() functions, removed append/0

--- ruz_exer05.erl
+++ ruz_exer05.erl
@@ -1,33 +1,16 @@
 -module(ruz_exer05).
--export([factors/1, append/2, safety/0]).
+-export([factors/1, safety/0]).
 
-factors(1) -> 													% base case for recursion
-	Factor_List = [1],
-	io:format("(base case) factors: ~p~n", [Factor_List]);		% use ~p to automagically print the list. Or anything. 
-
-factors(N) ->												% print a list of all the factors of N
-	Factor_List = [1],
-	Remainder = N rem (N-1),							    % find the remainder to determine if the value in 
-
-	io:format("N = ~p, (N-1) = ~p, Remainder: ~p~n", [N, N-1, Remainder]),
-	if
-		Remainder == 0 ->
-			io:format("here~n"),
-			append([Factor_List], (N-1)),					% append the value of N-1 to the list, error here tho
-			io:format("factors: ~p~n", [Factor_List]),
-			factors(N-1);
-		Remainder =/= 0 ->
-			io:format("then here~n"),
-			factors(N-1)
-	end.
-
-append([Head|Element], Tail) ->
-	[Head|append(Element, Tail)];
-append([], Tail) ->
-	Tail.
+factors(N) ->
+% used list comprehension after seeing sample factors() function on rosettacode.org
+% read this line as "The list of factors such that factors
+% is taken from the sequence of integers 1 to N, and N % factors = 0"
+	[Factors || Factors <- lists:seq(1,N), N rem Factors == 0].
 
 safety()->
 	io:format("wag ibenta ang bayan bc u aint a pussy like aguinaldo.~n").
 
 % References:
 %	http://erlang.org/doc/efficiency_guide/listHandling.html
+%	https://rosettacode.org/wiki/Factors_of_an_integer#Recursive
+%	http://erlang.org/doc/programming_examples/list_comprehensions.html 
